0d6e30caadf94f007a2bb07dd4c22ab863926223
sorokk <theo.soroka@hotmail.com>
2023-10-14 14:18:34 +0200

    ajout css, probleme sur le collapse, a regler, manque responsive et projet fini

--- my-app/src/components/Pages/Article/Collapsear.js
+++ my-app/src/components/Pages/Article/Collapsear.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import "./Collapsear.css";
 
 function Appartment(props) {
   const [isOpenDescription, setIsOpenDescription] = useState(false);
@@ -11,14 +12,14 @@ function Appartment(props) {
   const toggleEquipements = () => {
     setIsOpenEquipements(!isOpenEquipements);
   };
-
+  
   const data = props.data;
   const starArray = Array.from({ length: data.rating }, (_, index) => index + 1);
   
   return (
     <div className="apartment">
-    <h2>{data.title}</h2>
-    <p>{data.location}</p>
+    <h2 className='title-article'>{data.title}</h2>
+    <p className='location-article'>{data.location}</p>
     <div>
     {data.tags.map((tags, index) => (
       <span key={index} className="tag">
@@ -27,9 +28,9 @@ function Appartment(props) {
       ))}
       </div>
       <div className="host">
-      <img src={data.host.picture} alt="photo de profil du host" />
+      <img src={data.host.picture} alt="photo de profil du host" className='host-picture' />
       <div className="host-info">
-      <h3>{data.host.name}</h3>
+      <h3 className='host-name'>{data.host.name}</h3>
       <div className="rating">
       {starArray.map((star) => (
         <span key={star} className="star">&#9733;</span>
@@ -37,10 +38,11 @@ function Appartment(props) {
         </div>
         </div>
         </div>
-        <div className="collapse-item">
-        <p>Description</p>
+        <div className='collapse-container'>
+        <div className="collapse-item-article">
+        <p className='collapse-title'>Description</p>
         <button
-        className={`collapse-button ${isOpenDescription ? "open" : ""}`}
+        className={`collapse-button-article ${isOpenDescription ? "open" : ""}`}
         onClick={toggleDescription}
         >
         {isOpenDescription ? "▼" : "▲"}
@@ -51,13 +53,15 @@ function Appartment(props) {
           className={`text ${isOpenDescription ? "open" : ""}`}
           onClick={toggleDescription}
           >
+            <div className='collapse-description'>
           {data.description}
+          </div>
           </p>
           )}
-          <div className="collapse-item">
-          <p>Équipements</p>
+          <div className="collapse-item-article">
+          <p className='collapse-title'>Équipements</p>
           <button
-          className={`collapse-button ${isOpenEquipements ? "open" : ""}`}
+          className={`collapse-button-article ${isOpenEquipements ? "open" : ""}`}
           onClick={toggleEquipements}
           >
           {isOpenEquipements ? "▼" : "▲"}
@@ -68,9 +72,12 @@ function Appartment(props) {
             className={`text ${isOpenEquipements ? "open" : ""}`}
             onClick={toggleEquipements}
             >
+              <div className='collapse-description'>
             {data.equipments}
+            </div>
             </p>
             )}
+            </div>
             </div>
             );
           };
